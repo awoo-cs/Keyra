e2bca414342a1989c371742700d9cdf945950cd3
leo <131321518+awoo-cs@users.noreply.github.com>
2024-03-07 01:56:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+(function() {
+  var codeTime, commentTime, finalStyle, isOn, openComment, skip, styles, writeStyleChar, writeStyles;
+
+  styles = "/*\n * \"Keyra 🤍\" v0.0.1\n * awoo\n */\n\nbody {\n  background-color: #1a1c24; color: #fff;\n  font-size: 1.0rem; line-height: 1.4;\n  -webkit-font-smoothing: subpixel-antialiased;\n}\n\n/*\n * ...\n * ...hola?\n * estimada señorita Keyra Briggith~\n * no se por donde empezar\n * ha pasado unos cuantos dias desde que nos conocimos~\n * quien diria que se podrian hacer cosas como estas~\n * actualmente ambos salimos de cosas delicadas\n * pero no fueron imposibles de superar~\n * me gustaria mucho poder ayudarte y estar ahi con vos\n * se que pronto se nos dara\n * por el momento quiero darte este regalo\n * que aunque no signifique mucho\n * al menos tendra un lugar en tu corazon, y estare tranquilo\n * nunca cambies, porque sos perfecta tal cual y como sos ahora\n */\n\npre {\n  position: fixed;\n  top: 30px; bottom: 30px;\n  transition: left 500ms;\n  overflow: auto;\n  background-color: #313744; color: #a6c3d4;\n  border: 1px solid rgba(0,0,0,0.2);\n  padding: 24px 12px;\n  box-sizing: border-box;\n  border-radius: 3px;\n  box-shadow: 0px 4px 0px 2px rgba(0,0,0,0.1);\n}\n\n/*\n * ahora mismo lo que veras es un corazon\n * el mismo que vos me robaste con tu persona\n * y el cual te entrego porque late mucho por vos~\n */\n\npre em:not(.comment) { font-style: normal; }\n\n.comment       { color: #707e84; }\n.selector      { color: #c66c75; }\n.selector .key { color: #c66c75; }\n.key           { color: #c7ccd4; }\n.value         { color: #d5927b; }\n\n\n/*\n * empezando por lo mas minimo！\n * donde sea que lo estes viendo, el proposito es el mismo\n * solo observa señorita\n */\n\n@media screen and (max-width: 768px) {\n    pre { left: 6%;right: 6%;top: 50%; }\n}\n\n@media screen and (min-width: 768px) {\n    pre { width: 48%;left: 50%;bottom: 30px; }\n}\n\n/*\n * te quiero\n */\n\n@media screen and (max-width: 768px) {\n    #heart, #echo {\n        position: fixed;\n        width: 300px; height: 300px;\n        margin: 30px auto;\n        left: 0; right: 0;\n        text-align: center;\n        -webkit-transform: scale(0.95);\n        transform: scale(0.95);\n    }\n}\n\n@media screen and (min-width: 768px) {\n    #heart, #echo {\n      position: fixed;\n      width: 300px; height: 300px;\n      top: calc(50% - 150px); left: calc(25% - 150px);\n      text-align: center;\n      -webkit-transform: scale(0.95);\n              transform: scale(0.95);\n    }\n}\n\n#heart { z-index: 8; }\n#echo  { z-index: 7; }\n\n#heart::before, #heart::after, #echo::before, #echo::after {\n    content: '';\n    position: absolute;\n    top: 40px;\n    width: 150px; height: 240px;\n    background: #c66c75;\n    border-radius: 150px 150px 0 0;\n    -webkit-transform: rotate(-45deg);\n            transform: rotate(-45deg);\n    -webkit-transform-origin: 0 100%;\n            transform-origin: 0 100%;\n}\n\n#heart::before, #echo::before {\n  left: 150px;\n}\n\n#heart::after, #echo::after {\n  left: 0;\n  -webkit-transform: rotate(45deg);\n          transform: rotate(45deg);\n  -webkit-transform-origin: 100% 100%;\n          transform-origin: 100% 100%;\n}\n\n/* algo de sombra no viene mal, cierto?*/\n\n#heart::after {\n  box-shadow:\n    inset -6px -6px 0px 6px rgba(255,255,255,0.1);\n}\n\n#heart::before {\n  box-shadow:\n    inset 6px 6px 0px 6px rgba(255,255,255,0.1);\n}\n\n/*\n * un corazon~\n * a quien le pertenecera?\n * mmm\n */\n\n#heart i::before {\n  content: 'Keyra';\n  position: absolute;\n  z-index: 9;\n  width: 100%;\n  top: 35%; left: 0;\n  font-style: normal;\n  color: rgba(255,255,255,0.8);\n  font-weight: 100;\n  font-size: 30px;\n  text-shadow: -1px -1px 0px rgba(0,0,0,0.2);\n}\n\n/*\n * no es un corazon si no late, cierto?~\n * mira\n */\n\n@-webkit-keyframes heartbeat {\n  0%, 100% {\n    -webkit-transform: scale(0.95);\n            transform: scale(0.95);\n  }\n  50% {\n    -webkit-transform: scale(1.00);\n            transform: scale(1.00);\n  }\n}\n\n@keyframes heartbeat {\n  0%, 100% { transform: scale(0.95); }\n  50%      { transform: scale(1.00); }\n}\n\n@-webkit-keyframes echo {\n  0%   {\n    opacity: 0.1;\n    -webkit-transform: scale(1);\n            transform: scale(1);\n  }\n  100% {\n    opacity: 0;\n    -webkit-transform: scale(1.4);\n            transform: scale(1.4);\n  }\n}\n\n@keyframes echo {\n  0%   {\n    opacity: 0.1;\n    transform: scale(1);\n  }\n  100% {\n    opacity: 0;\n    transform: scale(1.4);\n  }\n}\n\n/*\n * veamoslo palpitar\n */\n\n#heart, #echo {\n  -webkit-animation-duration: 2000ms;\n          animation-duration: 2000ms;\n  -webkit-animation-timing-function:\n    cubic-bezier(0, 0, 0, 1.74);\n          animation-timing-function:\n            cubic-bezier(0, 0, 0, 1.74);\n  -webkit-animation-delay: 500ms;\n          animation-delay: 500ms;\n  -webkit-animation-iteration-count: infinite;\n          animation-iteration-count: infinite;\n  -webkit-animation-play-state: paused;\n          animation-play-state: paused;\n}\n\n#heart {\n  -webkit-animation-name: heartbeat;\n          animation-name: heartbeat;\n}\n#echo {\n  -webkit-animation-name: echo;\n          animation-name: echo;\n}\n\n/*\n * lista?~！\n */\n\n#heart, #echo {\n\n/*\n * ...3...\n *\n * ...2...\n *\n * ...1...\n *\n * espera un minuto~\n *\n */\n\n  -webkit-animation-play-state: running;\n          animation-play-state: running;\n\n/*\n * ...ahi viene!\n */\n\n}\n\n/*\n * epaa~~\n * asi mismo actua mi corazon cuando estas vos~\n * te gusto?~ espero se vea bien~~\n * no es la cosa mas impresionante, pero quise tener algun detalle con vos\n * hace mucho lo dije, pero me alegra mucho haberte conocido ahora\n *\n */\n\n/*\n * \sos como una llave que me cayo de la nada, en el momento menos oportuno\n * abriendome puertas hacia un mundo lleno de oportunidades\n * te dedico este pequeño detalle\n * no dejes de ser la estrella en la noche nublada\n * me encanta el rastro de esperanza y deseo que vas dejando cada dia que pasa\n */";
+
+  finalStyle = styles;
+
+  openComment = false;
+
+  isOn = true;
+
+  writeStyleChar = function(which) {
+    if (which === '/' && openComment === false) {
+      openComment = true;
+      styles = $('#style-text').html() + which;
+    } else if (which === '/' && openComment === true) {
+      openComment = false;
+      styles = $('#style-text').html().replace(/(\/[^\/]*\*)$/, '<em class="comment">$1/</em>');
+    } else if (which === ':') {
+      styles = $('#style-text').html().replace(/([a-zA-Z- ^\n]*)$/, '<em class="key">$1</em>:');
+    } else if (which === ';') {
+      styles = $('#style-text').html().replace(/([^:]*)$/, '<em class="value">$1</em>;');
+    } else if (which === '{') {
+      styles = $('#style-text').html().replace(/(.*)$/, '<em class="selector">$1</em>{');
+    } else {
+      styles = $('#style-text').html() + which;
+    }
+    $('#style-text').html(styles);
+    return $('#style-tag').append(which);
+  };
+
+  writeStyles = function(message, index, interval) {
+    var pre;
+    if (index < message.length) {
+      pre = document.getElementById('style-text');
+      pre.scrollTop = pre.scrollHeight;
+      writeStyleChar(message[index++]);
+      if (isOn) {
+        return setTimeout((function() {
+          return writeStyles(message, index, openComment ? commentTime : codeTime);
+        }), interval);
+      }
+    }
+  };
+
+  skip = function() {
+    isOn = false;
+    return setTimeout((function() {
+      var pre;
+      $('#style-text').html(finalStyle);
+      $('#style-tag').html(finalStyle);
+      pre = document.getElementById('style-text');
+      return pre.scrollTop = pre.scrollHeight;
+    }), 2 * commentTime);
+  };
+
+  $('body').append("  <style id=\"style-tag\"></style>\n<span id=\"echo\"></span>\n<span id=\"heart\"><i></i></span>\n<pre id=\"style-text\"></pre>\n  <div style = 'position: fixed;left: 30px;bottom: 10px;'>\n    <a id='skipAnimation'>Omitir</a>\n  </div>");
+
+  commentTime = 60;
+
+  codeTime = 20;
+
+  $(document).on('ready page:load', function() {
+    return $('#skipAnimation').click(function() {
+      return skip();
+    });
+  });
+
+  writeStyles(styles, 0, commentTime);
+
+}).call(this);
